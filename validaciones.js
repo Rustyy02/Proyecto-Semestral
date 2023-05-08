e083509014eb69cde53b4290b67e4bd744a3e2e1
Alvaro Figueroa <alv.figueroa@duocuc.cl>
2023-05-07 23:13:32 -0400

    Comienzo api clima
    api clima

--- validaciones.js
+++ validaciones.js
@@ -88,13 +88,15 @@ formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+
+	if(campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
 		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+			document.getElementById('formulario__mensaje-exito').classList.remove('mensaje-error');
 		}, 5000);
+
 
 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
 			icono.classList.remove('formulario__grupo-correcto');
